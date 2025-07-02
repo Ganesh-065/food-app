2b9f2319cba031a611f812e12a9d31c50b4c1b58
Ganesh-065 <ganesh.p@aifalabs.com>
2025-07-02 14:36:21 +0530

    updated backend env

--- server/index.ts
+++ server/index.ts
@@ -21,29 +21,35 @@ config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// // Middleware
-// app.use(
-//   cors({
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//     credentials: false,
-//   })
-// );
-
-const allowedOrigins = ["https://food-app-liart-xi.vercel.app/", "http://localhost:5173"];
-
+// Middleware
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: false, // Set to true if you want to allow credentials (cookies, authorization headers, etc.)
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    credentials: true, // if you're using cookies/auth
+    allowedHeaders: [
+      "Content-Type",   
+      "Authorization",
+      "Access-Control-Allow-Origin",
+    ],
   })
 );
+
+// const allowedOrigins = ["https://food-app-liart-xi.vercel.app/", "http://localhost:5173"];
+
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     credentials: true, // if you're using cookies/auth
+//   })
+// );
 
 app.options("*", cors()); // 👈 handles OPTIONS requests globally
 app.use(express.json());
